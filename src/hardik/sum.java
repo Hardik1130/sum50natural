7af670ca03ce1d1aa1150270fba71566452ed078
hardikgupta <hardikgupta176@gmail.com>
2022-09-24 18:42:01 +0530

    Initial version

---
+++ src/hardik/sum.java
@@ -0,0 +1,17 @@
+package hardik;
+
+public class sum {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int i,sum=0;
+        for(i=1; i<=50; i++)
+        {
+            sum += i;
+        }
+
+        System.out.print("The Sum of Natural numbers from 1 to 50 = "+sum);
+    }
+
+}
+
